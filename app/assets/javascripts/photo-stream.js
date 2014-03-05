342a780b7201f75cc88d6d54f37bf981068d7cf1
Makis Otman <makisotman@gmail.com>
2014-03-05 15:02:32 +0000

    Each photo and description are collected as a section

--- app/assets/javascripts/photo-stream.js
+++ app/assets/javascripts/photo-stream.js
@@ -8,9 +8,11 @@ var pusher = new Pusher('58cfbb27046afbc265f1');
 var channel = pusher.subscribe('the_force');
 
 channel.bind('new_photo', function(data) {
-  var photoStream = $('#photo-stream').first()
+  var photoStream = $('#photo-stream').first();
   if (photoStream) {
+    var newSection = $('<section>');
     var img = $('<img>').attr('src', data.url);
-    photoStream.prepend(data.description).prepend(img);
+    newSection.append(img).append(data.description);
+    photoStream.prepend(newSection);
   }
 });
