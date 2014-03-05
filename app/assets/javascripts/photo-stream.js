762fdb0137f190f34c5a405bc79136f3848c96cf
Makis Otman <makisotman@gmail.com>
2014-03-05 14:43:09 +0000

    modified Pusher code to live upload photos

--- app/assets/javascripts/photo-stream.js
+++ app/assets/javascripts/photo-stream.js
@@ -1,11 +1,16 @@
 Pusher.log = function(message) {
-      if (window.console && window.console.log) {
-        window.console.log(message);
-      }
-    };
+  if (window.console && window.console.log) {
+    window.console.log(message);
+  }
+};
 
-    var pusher = new Pusher('58cfbb27046afbc265f1');
-    var channel = pusher.subscribe('the_force');
-    channel.bind('new_photo', function(data) {
-      alert(data);
-    });
+var pusher = new Pusher('58cfbb27046afbc265f1');
+var channel = pusher.subscribe('the_force');
+
+channel.bind('new_photo', function(data) {
+  var photoStream = $('#photo-stream').first()
+  if (photoStream) {
+    var img = $('<img>').attr('src', data.url);
+    photoStream.prepend(data.description).prepend(img);
+  }
+});
