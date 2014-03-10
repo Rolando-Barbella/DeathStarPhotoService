8fc03fad9d17dc2864b95b2b1ef0e41d05e0aa7c
Peter <peterhsaxton@gmail.com>
2014-03-10 11:29:13 +0000

    Rspec test for validating presence of username and name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates_uniqueness_of :username
+  validates_presence_of :username, :name
 end
